b0945bcb6d933d06e881e4b97b3230b7ecb4dccb
marcos <marcospaulo.dev@hotmail.com>
2022-07-21 11:12:57 -0300

    corrigindo importação

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,4 @@
-export * from './Form/Form'
+export * from './form/Form'
 export * from './DrawerLeft/DrawerLeft'
 export * from '../../pages/Home/CardInfos'
 export * from '../../pages/Home2/CardInfos2'
