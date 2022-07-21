2fa2ad42d68ea57c5973d81bc1fc12ee1e3aa898
marcos <marcospaulo.dev@hotmail.com>
2022-07-20 21:19:24 -0300

    Adição do PWA

---
+++ public/pwa.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" sizes="512x512" href="./images/logo.png">
+        <link rel="manifest" href="./manifest.webmanifest">    
+        <script src="./index2.js"></script>
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="application-name" content="PWA Workshop">
+        <meta name="apple-mobile-web-app-title" content="PWA Workshop">
+    </head>
+
+    <body>
+
+    </body>
+</html>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,9 @@
 
 import { Button } from "@mui/material"
 import { Routes, Route, Navigate } from "react-router-dom"
-import { useAppThemeContext } from "../shared/contexts"
 
 
 export const AppRoutes = () => {
-    const { toggleTheme } = useAppThemeContext();
     return (
         <Routes>
             <Route path="/DartSystem"/>
